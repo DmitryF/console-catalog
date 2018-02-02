b929c5b492bd002f0bd5b95d08d5e82f508c18eb
DzmitryF <dzmitryf.developer@gmail.com>
2018-02-02 18:37:49 +0300

    RSS base classes

---
+++ src/com/rss/write/RSSWriteException.java
@@ -0,0 +1,14 @@
+package com.rss.write;
+
+/**
+ * @author DzmitryF
+ *
+ */
+public class RSSWriteException extends RuntimeException {
+
+	public RSSWriteException(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
